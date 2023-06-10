8b94b2fef3d850a0d1bb293c53eeebb90d1e98ba
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:21:12 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -1,5 +1,5 @@
 Events.on(ContentInitEvent, e => {
-    Vars.content.planet("asteroidgen-alter").generator = new ErekirPlanetGenerator(); //to see if the script being executed. 
+    Vars.content.planet("asteroidgen-alter").generator = space_to_coreZone; //to see if the script being executed. 
 });
 
 var space_to_coreZone = extend(AsteroidGenerator,{generate(){}}); 
