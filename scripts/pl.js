4aed4ec94a4c68818e4c3823e82d3c00392d25ef
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:42:11 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -5,7 +5,7 @@ Events.on(ContentInitEvent, e => {
 var space_to_coreZone = extend(AsteroidGenerator,{generate()
 {
     var seed = 1082;
-    var sx = width/2, sy = height/2;
+    var sx = 250, sy = 250;
     var rand = new Rand(seed);
     var background = Blocks.empty.asFloor(); //tiles.eachTile(t -> t.setFloor(background));
 }}); 
