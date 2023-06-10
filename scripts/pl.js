f55e96e396a27657d03695fd0792e2f8b1e7a448
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:09:43 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -1,22 +1,3 @@
 Events.on(ContentInitEvent, e => {
     Vars.content.planet("asteroidgen-alter").generator = new ErekirPlanetGenerator(); //to see if the script being executed. 
 });
-/*
-var replace_space_with_coreZone = extend(AsteroidGenerator,
-{
-    generate()
-    {
-        var background = Blocks.empty.asFloor(); var i:int = 0;
-        var seed = state.rules.sector.planet.id;
-        var sx:int = width/2, sy = height/2;
-        var ra = new Rand(seed); var amount:int = ra.random(min, max);
-        
-        while(i < amount)
-        {
-            var radius = rand.random(radMin, radMax); var ax = rand.random(radius, width - radius); var ay = rand.random(radius, height - radius);
-            i = i+1;
-        }
-    }
-});
-function ast_sectorSize_changed(size){var b=extend(AsteroidGenerator,{getSectorSize(sector){return size}});}
-*/
