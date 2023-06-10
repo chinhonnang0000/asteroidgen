8854a539ff967f3aed6d0298a0c8654b6564ba40
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:14:18 +0800

    Update pl.js
    to see if this script is ignored

--- scripts/pl.js
+++ scripts/pl.js
@@ -1,3 +1,5 @@
 Events.on(ContentInitEvent, e => {
     Vars.content.planet("asteroidgen-alter").generator = new ErekirPlanetGenerator(); //to see if the script being executed. 
 });
+
+var space_to_coreZone = extend(AsteroidGenerator,generate(){});
