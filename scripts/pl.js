42c063b8a595f0f8037461700c3bc43f4df10ddc
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:38:18 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -2,13 +2,11 @@ Events.on(ContentInitEvent, e => {
     Vars.content.planet("asteroidgen-alter").generator = space_to_coreZone(213123); //to see if the script being executed. 
 });
 
-function space_to_coreZone(seed_int)
-{ 
-    var astg = extend(AsteroidGenerator,{generate()
-    {
-        var seed = seed_int;
-        var sx = width/2, sy = height/2;
-        var rand = new Rand(seed);
-        var background = Blocks.empty.asFloor(); //tiles.eachTile(t -> t.setFloor(background));
-    }}); 
-}
+var space_to_coreZone = extend(AsteroidGenerator,{generate()
+{
+    var seed = 1082;
+    var sx = width/2, sy = height/2;
+    var rand = new Rand(seed);
+    var background = Blocks.empty.asFloor(); //tiles.eachTile(t -> t.setFloor(background));
+}}); 
+
