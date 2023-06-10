19b1095190d5939839e0b2fa367eed0ac3f8d265
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 23:18:07 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -2,4 +2,4 @@ Events.on(ContentInitEvent, e => {
     Vars.content.planet("asteroidgen-alter").generator = new ErekirPlanetGenerator(); //to see if the script being executed. 
 });
 
-var space_to_coreZone = extend(AsteroidGenerator,generate(){});
+var space_to_coreZone = extend(AsteroidGenerator,{generate(){}}); causes the javascript to be ignored. 
