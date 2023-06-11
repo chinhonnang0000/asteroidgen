181d84ce12833ed09fa9c888e1de85eb4875fc8e
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-11 16:14:39 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -3,22 +3,13 @@ Events.on(ContentInitEvent, e => {
 });
 
 var space_to_coreZone = extend(AsteroidGenerator,{
-    asteroid(x,y,z)
-    {
-        var floor = (rand.chance(iceChance) ? Blocks.ice :
-        rand.chance(carbonChance) ? Blocks.carbonStone :
-        rand.chance(berylChance) ? Blocks.beryllicStone :
-        rand.chance(ferricChance) ? Blocks.ferricStone:Blocks.stone
-        ).asFloor();
-    },
-    
     generate()
     {
         var seed = 1082; var width=500; var height=500;    
         var sx = 250, sy = 250;
         var rand = new Rand(seed);
         var background = Blocks.empty.asFloor(); //tiles.eachTile(t -> t.setFloor(background));
-        asteroid(sx, sy, rand.random(30, 50));
+        //asteroid(sx, sy, rand.random(30, 50));// does not work
         var amount = rand.random(min, max);
     }
 }); 
