b91850513087acae4730d658ad6c8fe4488fe0c7
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-10-04 15:46:18 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -1,2 +1,9 @@
+function init_random()
+{
+  
+}
+var ast_main_generator = extend(AsteroidGenerator,{}) // this will add carbon vent to the asteroidgen main planet. 
+{
+}
 
-
+var seed_vcar, dist_vcar; 
